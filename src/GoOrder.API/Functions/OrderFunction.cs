b0ee876696157f6e900cb00ea2a052226a07f1c3
OzturkVedat <ozturkvedat72@gmail.com>
2025-02-21 08:33:52 +0300

    Add project files.

---
+++ src/GoOrder.API/Functions/OrderFunction.cs
@@ -0,0 +1,10 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace GoOrder.API.Functions
+{
+    internal class OrderFunction
+    {
+    }
+}
---
+++ src/GoOrder.API/Services/OrderService.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+namespace GoOrder.API.Services
+{
+    internal class OrderService
+    {
+        public async Task<APIGatewayHttpApiV2ProxyResponse> CreateOrder(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
