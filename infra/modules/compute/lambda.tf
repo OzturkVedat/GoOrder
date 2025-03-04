efd3062d736930e8738795ae294373edb9e2a9da
OzturkVedat <ozturkvedat72@gmail.com>
2025-03-04 13:07:30 +0300

    s3 added again

--- infra/modules/compute/lambda.tf
+++ infra/modules/compute/lambda.tf
@@ -2,11 +2,12 @@
 resource "aws_lambda_function" "order_service" {
   function_name = "OrderServiceLambda"
   role          = aws_iam_role.lambda_exe_role.arn
-  package_type  = "Image"
-  image_uri     = var.lambda_api_image_uri
   timeout       = 30
   memory_size   = 512
   publish       = false # true, if you want versioning
+
+  s3_bucket = var.lambda_bucket_name
+  s3_key    = var.lambda_bucket_key
 
   vpc_config {
     subnet_ids         = [var.private_subnet_id]
