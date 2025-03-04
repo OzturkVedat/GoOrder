cdf37a9ea14bb21e57458a0f36bef9f6eabeac41
OzturkVedat <ozturkvedat72@gmail.com>
2025-03-04 13:03:22 +0300

    s3 removed, fix

--- infra/modules/compute/lambda.tf
+++ infra/modules/compute/lambda.tf
@@ -8,9 +8,6 @@ resource "aws_lambda_function" "order_service" {
   memory_size   = 512
   publish       = false # true, if you want versioning
 
-  s3_bucket = var.lambda_bucket_name
-  s3_key    = var.lambda_bucket_key
-
   vpc_config {
     subnet_ids         = [var.private_subnet_id]
     security_group_ids = [var.lambda_sg_id]
